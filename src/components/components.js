d8f3ce4f3e3d3809dfb155da2f7988078e2f7386
Nikolas81 <riazanovna@mail.ru>
2024-02-07 15:32:52 +0300

    addComponents

--- src/components/components.js
+++ src/components/components.js
@@ -1,7 +1,11 @@
 import MainPage from './pages/MainPage';
+import SignupPage from './pages/SignupPage';
+import LoginPage from './pages/LoginPage';
 
 const components = {
   MainPage,
+  SignupPage,
+  LoginPage,
 };
 
 export default components;
--- src/routes/apiRouter.js
+++ src/routes/apiRouter.js
@@ -11,15 +11,15 @@ router.get('/', (req, res) => {
 });
 
 router.post('/signup', async (req, res) => {
-  const { name, email, password } = req.body;
+  const { username, email, password } = req.body;
 
-  if (!(name && email && password)) {
+  if (!(username && email && password)) {
     return res.status(400).json({ message: 'Все поля должны быть заполнены' });
   }
 
   const [user, created] = await User.findOrCreate({
     where: { email },
-    defaults: { name, password: await bcrypt.hash(password, 10) },
+    defaults: { username, password: await bcrypt.hash(password, 10) },
   });
 
   if (!created) return res.status(403).json({ message: 'Такой пользователь уже создан' });
