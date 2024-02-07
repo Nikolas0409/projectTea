c4cfb3967bbbe004aab5a2b5044250a4a875bedb
beetleOf <beet1le@mail.ru>
2024-02-07 15:07:29 +0300

    navbar

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   const initState = { hello: 'world' };
-  res.render('IndexPage', initState);
+  res.render('MainPage', initState);
 });
 
 export default router;
